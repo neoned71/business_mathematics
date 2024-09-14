6d0975be4791601718009c7b6e2224137351b60b
neoned71 <neoned71@gmail.com>
2024-09-14 13:31:44 -0600

    added cheks for n

--- src/business_mathematics_neoned71/compound_interest.py
+++ src/business_mathematics_neoned71/compound_interest.py
@@ -2,6 +2,8 @@ import math
 
 # Total amount at the end of term
 def final_amount(p,r,t,n=1):
+    if(n==0):
+        n=1
     return p*((1+r/n)**(t*n))
 
 # names of the function should be sufficient in describing what the function does.
@@ -10,14 +12,20 @@ def interest_amount(p,r,t,n=1):
 
 # Get the principal value from the final amount, rate of interest and time
 def reverse_P_from_FA(fa,r,t,n=1):
+    if(n==0):
+        n=1
     return fa/((1+r/n)**(t*n))
 
 # Get the rate of interest from the final amount, principal value and time
 def reverse_R_from_FA(fa,p,t,n=1):
+    if(n==0):
+        n=1
     return n*((fa/p)**(1/(n*t))-1)
 
 # Get the time from the final amount, rate of interest and principal value
 def reverse_T_from_FA(fa,p,r,n=1):
+    if(n==0):
+        n=1
     return (1/n)*math.log(fa/p)/math.log(1+r/n)
 
 
